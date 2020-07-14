ffe5274f557addf26fd5897d58ca45892a95b39c
eullaguari <eullaguari>
2020-07-13 22:32:39 -0500

    add parser

--- ProyectoLP/ruby_lex.py
+++ ProyectoLP/ruby_lex.py
@@ -13,11 +13,10 @@ tokens = [
 	'SYMBOL', # variable names
 	'SYMBOL_UPPER', # variable names
 	'NUMBER',
-	'COMILLA_SIMPLE',
-	'COMILLA_DOBLE',
 	'TEXT',
 	'DECIMAL',
  	'EQUAL',
+ 	'ASIGN',
 	'GT',
 	'LT',
 	'GEQT',
@@ -25,11 +24,12 @@ tokens = [
 	'FUNCTION',
 	'COMMENT',
 	'COMMA',
+	'DDOT',
 	'DOT',
 ]
 
 t_PLUS = r'\+'
-t_MINUS = r'-'
+t_MINUS = r'\-'
 t_TIMES = r'\*'
 t_DIVIDED = r'/'
 t_LPAREN = r'\('
@@ -38,18 +38,18 @@ t_LLLAVE = r'\{'
 t_RPAREN = r'\)'
 t_RBRA = r'\]'
 t_RLLAVE = r'\}'
-t_SYMBOL = r'[a-z]\w*'
-t_SYMBOL_UPPER = r'[A-Z]\w*'
-t_COMILLA_SIMPLE = r"\'"
-t_COMILLA_DOBLE = r'\"'
+t_SYMBOL = r'\${0,1}[a-z]\w*'
+t_SYMBOL_UPPER = r'\${0,1}[A-Z]\w*'
 t_TEXT = r"(\'[\w\s\.]*\'|\"[\w\s\.]*\")"
-t_EQUAL = r'={1}'
+t_EQUAL = r'={2}'
+t_ASIGN = r'={1}'
 t_GT = r'>'
 t_LT = r'<'
 t_GEQT = r'>='
 t_LEQT = r'<='
 t_COMMENT = r'\#[\w\s\.]*'
 t_COMMA = r'\,'
+t_DDOT = r'\.\.'
 t_DOT = r'\.'
 
 reserved_words = {
@@ -67,16 +67,15 @@ reserved_words = {
 	'for': "FOR",
 	'when': "WHEN",
 	'while': "WHILE",
-	'if': 'IF'
-}
-
-# reserved words
-functions = {
+	'if': 'IF',
+	'elsif': 'ELSIF',
+	'else': 'ELSE',
 	'puts': "PUTS",
-	'def': "DEF"
+	'def': "DEF",
+	'unless': "UNLESS"
 }
 
-tokens = tokens + list(functions.values()) +  list(reserved_words.values())
+tokens = tokens +  list(reserved_words.values())
 
 def t_DECIMAL(t):
 	r'\d+\.\d+'
@@ -96,8 +95,6 @@ def t_ID(t):
 	r'([a-z\-0-9]+)'
 	if t.value in reserved_words:
 		t.type = reserved_words[t.value]
-	elif t.value in functions:
-		t.type = 'FUNCTION'
 	else:
 		t.type = 'SYMBOL'
 	return t
